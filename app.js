63a8ed5c37e889d96bcad66a54053c7fd3a1342e
Ranjit67 <ranjitsahoo873@gmail.com>
2021-05-04 21:14:09 +0530

    C:/Program Files/Git/

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const http = require("http");
 const server = http.createServer(app);
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
   },
 });
 const requestList = {};
